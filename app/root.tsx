cd7a0479477a19a227a2e7e9c9897fb3e75f9262
Yaroslav Lukovetskyi <111358303+Yaroslav0102030405@users.noreply.github.com>
2025-06-10 23:48:00 +0300

    modern-normalizide

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import 'modern-normalize/modern-normalize.css';
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
